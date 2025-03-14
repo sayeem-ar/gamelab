5d154a71ea502a7774da7d0d19e313765c442fea
Adnan Rahman Sayeem <adnan.rahman.sayeem@mail.com>
2025-03-14 16:36:27 +0600

    is_board_full

--- Tic-Tac-Toe/tictactoe_PvP.py
+++ Tic-Tac-Toe/tictactoe_PvP.py
@@ -1,4 +1,5 @@
 import pygame, sys
+import numpy as np
 
 pygame.init()   # always need to initialize pygame if we use import pygame
 
@@ -6,6 +7,8 @@ pygame.init()   # always need to initialize pygame if we use import pygame
 WIDTH = 600
 HEIGHT = 600
 LINE_WIDTH = 15
+BOARD_ROWS = 3
+BOARD_COLS = 3
 
 # colors
 RED = (255, 0, 0)
@@ -17,10 +20,15 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Tic Tac Toe (PvP)')
 
 # Load and set the window icon
-icon = pygame.image.load(r'C:\Users\DarkBoyAR\Desktop\GitHub-sayeem-ar\gamelab\Tic-Tac-Toe\icon.png')  # Replace 'icon.png' with desired icon file
+icon = pygame.image.load(r'F:\ar_sayeem_github\gamelab\Tic-Tac-Toe\icon.png')  # Replace 'path' with desired icon file location
 pygame.display.set_icon(icon)
 
 screen.fill(BG_COLOR)
+
+# board
+board = np.zeros((BOARD_ROWS, BOARD_COLS))
+#print(board)
+
 
 # Function to draw the lines of the Tic Tac Toe grid
 def draw_lines():
@@ -30,6 +38,59 @@ def draw_lines():
     # vertical
     pygame.draw.line(screen, LINE_COLOR, (200, 0), (200, 600), LINE_WIDTH)
     pygame.draw.line(screen, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
+
+def mark_square(row, col, player):
+    board[row][col] = player
+
+def avainable_square(row, col):
+    return board[row][col] == 0       # [same as below comment]
+''' if board[row][col]:
+        return True
+    else:
+        return False
+'''
+
+def is_board_full():
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == 0:
+                return False
+    
+    return True
+
+# false
+print(is_board_full())
+
+#marking all squares
+for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            mark_square(row, col, 1)
+# board is full true
+print(is_board_full())
+
+
+
+
+# is the middle square abailable?
+# print(avainable_square(1, 1))
+# mark_square(1, 1, 1)                # (1, 1) spot taken by P1, so not available
+# print(avainable_square(1, 1))
+
+
+# mark_square(0, 0, 1)
+# mark_square(1, 1, 2)
+
+
+
+
+
+
+
+# print(board)
+
+
+
+
 
 draw_lines()
 
